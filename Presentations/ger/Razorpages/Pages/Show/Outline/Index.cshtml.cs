1139f27ec4eaed63c95cc0a9286027bd0591d3df
Jakub Mikos <jakubmikos@outlook.com>
2018-04-25 00:17:50 +0200

    Three Views

---
+++ Presentations/ger/Razorpages/Pages/Show/Outline/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages.Show.Outline {
+    public class PresentData : PageModel {
+
+        public readonly IEnumerable<string> _keyPointList;
+
+        public PresentData () {
+            this._keyPointList = new List<string> () {
+                "Klassen und Modelle in Razer Files möglich",
+                "Voller funktionsumfang von c#",
+                "Keine IDE unterstützung",
+                "Fehler erst zur Laufzeit"
+            };
+        }
+
+    }
+}
