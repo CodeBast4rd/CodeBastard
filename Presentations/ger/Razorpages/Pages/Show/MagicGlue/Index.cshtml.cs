3f614e507ea7a5cd665d8b8faa299edc75c498da
Jakub Mikos <j.mikos@bcid.de>
2018-04-25 13:19:50 +0200

    More Magic Glue

--- Presentations/ger/Razorpages/Pages/Show/MagicGlue/Index.cshtml.cs
+++ Presentations/ger/Razorpages/Pages/Show/MagicGlue/Index.cshtml.cs
@@ -4,17 +4,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPages.Pages.Show.MagicGlue {
-    public class GlueBrand : PageModel {
+  public class GlueBrand : PageModel {
 
-        [BindProperty] 
-        public string BrandName { get; set;}
-        
-        public GlueBrand(){
-            
-        }
+    [BindProperty]
+    public string BrandName { get; set; }
 
-        public async Task OnGetAsync(string name){
-            this.BrandName =name;
-        }
+    public IEnumerable<string> BrandNameExampelList { get; set; }
+
+    public GlueBrand() {
+      this.BrandNameExampelList = new List<string>() {
+        "Magic Glue",
+        "Silver Glue",
+        "Sticky Stick",
+        "Unicorn Clench"
+      };
     }
+
+    public async Task OnGetAsync(string name) {
+      this.BrandName = name;
+    }
+  }
 }
